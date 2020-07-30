bfc30587800b3996995fa3ef787c671516dad6bc
GCollica <49055809+GCollica@users.noreply.github.com>
2020-07-30 21:18:48 +0800

    Created Base Class for Basic Enemies, created BasicEnemy1 script. Various Sprite Imports
    As the title reads, created Class for Basic Enemies, along with a BasicEnemy1 script. Along with this I've imported and mocked up various Assets that Hugh has produced so far, creating folders for organisation with them.

---
+++ Assets/Scripts/GianProtoScripts/BasicEnemy1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy1 : MonoBehaviour
+{
+    /*Script that will be attached to each basic enemy 1 gameobject throughout the game. Holds individual values for damage, resistance, health & movement speed and feeds that into it's own instance of the BasicEnemyClass. */
+
+    public float damage;
+    public float resistance;
+    public float health;
+    public float movementSpeed;
+
+    private BasicEnemyClass basicEnemyClass;
+    
+    void Awake()
+    {
+        InitialiseClassInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* 
+            //Testing Take Damage Function from basicEnemyClass.
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log(basicEnemyClass.damage);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log(basicEnemyClass.health);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            basicEnemyClass.TakeDamage(basicEnemyClass.damage);
+        }
+        */
+    }
+
+    //Initialises an instance of the Basic Enemy Class, feeding it values for damage, resistance, health, movespeed as the constructor requires.
+    private void InitialiseClassInstance()
+    {
+        basicEnemyClass = new BasicEnemyClass(damage, resistance, health, movementSpeed);
+    }
+}
---
+++ Assets/Scripts/GianProtoScripts/BasicEnemyClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyClass
+{
+    /*Base class for all basic enemies in the game, holds values for Damage, Resistance, Current Health, Movement Speed. Also includes commonly used functions shared by all common enemies. Specific functions used by each enemy type individually will be housed in their BasicEnemyX scrpit instead.*/
+
+    public float damage;
+    public float resistance;
+    public float health;
+    public float movementSpeed;
+
+    public BasicEnemyClass(float damageInput, float resistanceInput, float healthInput, float movementSpeedInput)
+    {
+        damage = damageInput;
+        resistance = resistanceInput;
+        health = healthInput;
+        movementSpeed = movementSpeedInput;
+    }
+
+    //Calculates the damage taken given an input value against the resistance of the enemy. Then updates current health to accomodate. 
+    public void TakeDamage(float incomingDamage)
+    {
+        float resCalculated = (incomingDamage * (1 - (resistance / 100f)));
+        //Debug.Log(resCalculated);
+        health -= resCalculated;
+    }
+
+}
