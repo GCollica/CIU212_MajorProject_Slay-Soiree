03750431643e5760850be7b0c7dd48a177732bbe
MacVP <48820988+MacVP@users.noreply.github.com>
2020-08-06 22:21:38 +0800

    (Mac) Basic Player Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Player Movement Speed
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        //Movement on x axis
+        movement.x = Input.GetAxisRaw("Horizontal");
+        //Movement on y axis
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
