c1fa6219ce4d03965fe9e917e84e201a70aa052a
MacVP <48820988+MacVP@users.noreply.github.com>
2020-08-16 18:55:27 +0800

    (Mac) Edited character movement to use velocity instead of directional movement

---
+++ Assets/Scripts/MacProtoScripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Player Stats
+    private float playerSpeed; 
+    #endregion
+
+
+    //Player Movement Speed
+    public float basemoveSpeed;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        //Sets horizontal movement on the y axis and vertical movement 
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //if(Input.GetButtonDown)
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement * ((basemoveSpeed + playerSpeed) * 100) * Time.fixedDeltaTime;
+    }
+}
