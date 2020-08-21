e88069d94d798918ede7388c08df7f2987502715
MacVP <48820988+MacVP@users.noreply.github.com>
2020-08-21 23:31:02 +0800

    (Mac) Character Controller with New Unity Input System

--- Assets/Scripts/MacProtoScripts/PlayerMovement.cs
+++ Assets/Scripts/MacProtoScripts/PlayerMovement.cs
@@ -4,28 +4,89 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    #region Player Stats
-    private float playerSpeed; 
-    #endregion
+    //Speed stat that changes with items
+    public float playerSpeed;
+    //Starting speed for player
+    public float baseSpeed;
 
+    //Unity auto-generated input script
+    private PlayerInput controls;
 
-    //Player Movement Speed
-    public float basemoveSpeed;
+    //For calculating movement for character
+    Vector2 move;
+    private Vector2 targetVelocity;
+    //Acceleration rate
+    public float forceMult;
 
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-    Vector2 movement;
-
-    void Update()
+    void Awake()
     {
-        //Sets horizontal movement on the y axis and vertical movement 
-        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb = GetComponent<Rigidbody2D>();
 
-        //if(Input.GetButtonDown)
+        //Input set up
+        controls = new PlayerInput();
+        
+        //Light attack
+        controls.Player.LightAttack.performed += context => LightAttack();
+        
+        //Movement with left joystick
+        controls.Player.Movement.performed += context => move = context.ReadValue<Vector2>();
+        controls.Player.Movement.canceled += context => move = Vector2.zero;
+
+        //Heavy attack
+        controls.Player.HeavyAttack.performed += context => HeavyAttack();
+
+        //Interact
+        controls.Player.Interact.performed += context => Interact();
+
+        //Item
     }
 
     void FixedUpdate()
     {
-        rb.velocity = movement * ((basemoveSpeed + playerSpeed) * 100) * Time.fixedDeltaTime;
+        //Assigns "m" to the Vector2 value of the left joystick axes
+        Vector2 m = new Vector2(move.x, move.y);
+        
+        //Sets the velocity to accelerate to
+        targetVelocity = m * ((baseSpeed + playerSpeed) * 100) * Time.fixedDeltaTime;
+        
+        //Calculates the amount of force delivered each frame
+        Vector2 force = (targetVelocity - rb.velocity) * forceMult;
+        
+        //Adds force
+        rb.AddForce(force);
+    }
+
+    void Interact()
+    {
+        Debug.Log("Interact!");
+    }
+
+    void LightAttack()
+    {
+        Debug.Log("Light Attack!");
+    }
+
+    void HeavyAttack()
+    {
+        Debug.Log("Heavy Attack!");
+    }
+
+    void Move(Vector2 direction)
+    {
+        Debug.Log("Moveing!" + direction);
+    }
+
+    //Enables controls when we need them
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    //Disables controls when we don't need them
+    void OnDisable()
+    {
+        controls.Disable();
     }
 }
